df80261c20f1bab3ec14dd6be828e7d82e64fc89
lelopereira05 <lelopereira05@hotmail.com>
2018-01-31 10:50:53 -0200

    Add files via upload

--- backend/api/billingCycle/billingCycle.js
+++ backend/api/billingCycle/billingCycle.js
@@ -10,16 +10,16 @@ const creditSchema = new mongoose.Schema({
 const debtSchema = new mongoose.Schema({
     name:{type: String, required: true},
     value: {type: Number, min: 0, required: true},
-    status: {type: String, required: false, uppercase: true},
-    enum: ['PAGO', 'PENDENTE', 'AGENDADO'] 
+    status: {type: String, required: false, uppercase: true,
+    enum: ['PAGO', 'PENDENTE', 'AGENDADO'] }
 })
 
 
 
 const billingCycleSchema = new mongoose.Schema({
     name:{type: String, required: true},
-    value: {type: Number, min: 1, max: 12, required: true},
-    year: {type: Number, min: 1970, max: 2020, required: true },
+    month: {type: Number, min: 1, max: 12, required: true},
+    year: {type: Number, min: 1970, max: 2020 },
     credits :[creditSchema],
     debts: [debtSchema] 
 })
