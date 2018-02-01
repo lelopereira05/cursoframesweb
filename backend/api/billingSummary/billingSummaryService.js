865469e132079ed6de3cb478505954b3bf6bd772
lelopereira05 <lelopereira05@hotmail.com>
2018-02-01 11:13:25 -0200

    Atualização billingSummaryService

--- backend/api/billingSummary/billingSummaryService.js
+++ backend/api/billingSummary/billingSummaryService.js
@@ -3,13 +3,12 @@ const BillingCycle = require('../billingCycle/billingCycle')
 
 
 function getSummary(req, res){
-    BillingCycle.aggregate({
-        $project:{credit: {$sum: "$credits.value"}, debt: {$sum: "$debts.value"}},
-       }, {
-       $group: {_id: null, credit:{$sum: "$credit"},debt: {$sum : "$debt"}}
-    },{
-        $project:{_id: 0, credit: 1, debt: 1}
-    }, function(error, result){
+    BillingCycle.aggregate([
+        {$project:{credit: {$sum: "$credits.value"}, debt: {$sum: "$debts.value"}} }, 
+        { $group: {_id: null, credit:{$sum: "$credit"},debt: {$sum : "$debt"}} },
+        { $project:{_id: 0, credit: 1, debt: 1} }
+    ],
+     function(error, result){
      if(error){
          res.status(500).json({errors: [error]})
      }else{
@@ -19,3 +18,5 @@ function getSummary(req, res){
 }
 
 module.exports = { getSummary }
+
+
