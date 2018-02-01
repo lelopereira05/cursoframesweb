8e033deea1d6b8f9b6638fa5b0447f76065d3174
lelopereira05 <lelopereira05@hotmail.com>
2018-02-01 11:14:25 -0200

    Atualização e correções de Bugs

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,2 +1,8 @@
 const mongoose = require('mongoose')
-module.exports = mongoose.connect('mongodb://localhost:27017/db_finance')
+module.exports = mongoose.connect('mongodb://localhost/db_finance')
+
+
+mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
+mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
+mongoose.Error.messages.Number.max = "O '{VALUE}' informado é maior que o limite máxímo de '{MAX}'."
+mongoose.Error.messages.String.enum = "'{VALUE}' não é valido para o atributo '{PATH}'."
