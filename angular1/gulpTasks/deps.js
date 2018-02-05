44a98209574f4cb1b2b49c9a5e7dc7c7a43eaf0b
lelopereira05 <lelopereira05@hotmail.com>
2018-02-05 13:40:32 -0200

    Add files via upload

--- angular1/gulpTasks/deps.js
+++ angular1/gulpTasks/deps.js
@@ -1,5 +1,4 @@
 //arquivos de dependencias do angular
-
 const gulp = require('gulp')
 const uglify = require('gulp-uglify')
 const uglifycss = require('gulp-uglifycss')
@@ -10,7 +9,7 @@ gulp.task('deps', ['deps.js', 'deps.css', 'deps.fonts'])
 gulp.task('deps.js', () => {
     return gulp.src([
         'node_modules/angular/angular.min.js',
-        'node_modules/angular-ui-router/release/angular-ui-router.min.js',
+        'node_modules/@uirouter/angularjs/release/angular-ui-router.min.js',
         'node_modules/angular-animate/angular-animate.min.js',
         'node_modules/angular-toastr/dist/angular-toastr.tpls.min.js',
         'node_modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
@@ -29,7 +28,7 @@ gulp.task('deps.css', () => {
         'node_modules/font-awesome/css/font-awesome.min.css',
         'node_modules/admin-lte/bootstrap/css/bootstrap.min.css',
         'node_modules/admin-lte/dist/css/AdminLTE.min.css',
-        'node_modules/admin-lte/dist/css/skin/_all-skins.min.css',
+        'node_modules/admin-lte/dist/css/skins/_all-skins.min.css',
     ])
     .pipe(uglifycss({"uglyComments" : true}))
     .pipe(concat('deps.min.css'))
