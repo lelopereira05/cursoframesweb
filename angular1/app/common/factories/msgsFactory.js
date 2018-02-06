9674406d4b00174065a07fe1f80ceb67df8a405c
lelopereira05 <lelopereira05@hotmail.com>
2018-02-06 11:41:36 -0200

    Factory para exibição de mensagens

---
+++ angular1/app/common/factories/msgsFactory.js
@@ -0,0 +1,26 @@
+(function(){
+    angular.module('primeiraApp').factory('msgs', [
+        'toastr',
+        MsgsFactory
+    ]) 
+
+    function MsgsFactory(toastr){
+
+        function addMsg(msgs, title, method){
+            if(msgs instanceof Array){
+                msgs.forEach(msg => toastr[method](msg, title))
+            }else{
+                toastr[method](msgs, title)
+            }
+        }
+
+        function addSucess(msgs) {
+            addMsg(msgs, 'Sucesso', 'sucess')
+        }
+    
+        function addError(msgs){
+            addMsg(msgs, 'Erro', 'error')
+        }
+        return { addSucess, addError }  
+    }
+})()
