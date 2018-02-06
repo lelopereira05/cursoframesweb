e728dee8708f9cdfee0cc386154cbf5503fd59e9
lelopereira05 <lelopereira05@hotmail.com>
2018-02-06 11:43:44 -0200

    Add files via upload

--- backend/config/server.js
+++ backend/config/server.js
@@ -3,14 +3,12 @@ const port = 3003
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
+const allowCors = require('./cors')
 
-//Toda requisição que chegar dentro do meu backend passará por esse middleware (urlEncoded)
-//extended - interpretar mais tipos de informações na submissão de um formulário
 server.use(bodyParser.urlencoded({extended: true}))
-
-//dentro do ciclo de requisição, quando chegar no backend vai passar primeiro pelo urlencoded
-//em seguida passa pelo json, fazendo uma interpretação 
 server.use(bodyParser.json())
+
+server.use(allowCors)
 
 server.listen(port, function(){
    console.log(`BACKEND is running on port ${port}.`)
