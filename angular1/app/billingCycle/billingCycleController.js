dbd17020dae9812ff48b61e809f4467179eb4fc5
lelopereira05 <lelopereira05@hotmail.com>
2018-02-07 11:43:23 -0200

    Factory para Exibição das abas

--- angular1/app/billingCycle/billingCycleController.js
+++ angular1/app/billingCycle/billingCycleController.js
@@ -2,19 +2,19 @@
     angular.module('primeiraApp').controller('BillingCycleCtrl',[
        '$http',
        'msgs',
+       'tabs',
        BillingCycleController 
     ])
-    function BillingCycleController($http, msgs){
+    function BillingCycleController($http, msgs, tabs){
         const vm = this
         const url = 'http://localhost:3003/api/billingCycles'
         vm.refresh = function(){
             $http.get(url).then(function(response){
                 vm.billingCycle = {}
                 vm.billingCycles = response.data
+                tabs.show(vm, { tabList : true, tabCreate: true })
             })
         }
-
-
 
         vm.create = function(){
             $http.post(url, vm.billingCycle).then(function(response){
