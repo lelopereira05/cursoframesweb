906bc4d1d21cdad098a7a6ceff7ac175e814e678
lelopereira05 <lelopereira05@hotmail.com>
2018-02-15 09:38:37 -0200

    Add files via upload

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,15 +1,25 @@
 const express = require('express')
-
-module.exports = function(server){
-    const router = express.Router()
-    server.use('/api', router)
-// router.route('/teste').get(function(req, res, next){
-//     res.send('funcionou')
-
-//    //rotas da api
-   const billingCycleService = require('../api/billingCycle/billingCycleService')
-   billingCycleService.register(router, '/billingCycles')
-
-   const billingSummaryService = require('../api/billingSummary/billingSummaryService')
-   router.route('/billingSummary').get(billingSummaryService.getSummary)
+const auth = require('./auth')
+module.exports = function (server) {
+    /*
+    * Rotas abertas
+    */
+    const openApi = express.Router()
+    server.use('/oapi', openApi)
+    const AuthService = require('../api/user/authService')
+    openApi.post('/login', AuthService.login)
+    openApi.post('/signup', AuthService.signup)
+    openApi.post('/validateToken', AuthService.validateToken)
+    /*
+    * Rotas protegidas por Token JWT
+    */
+    const protectedApi = express.Router()
+    server.use('/api', protectedApi)
+    protectedApi.use(auth)
+    const billingCycleService = require(
+        '../api/billingCycle/billingCycleService')
+    billingCycleService.register(protectedApi, '/billingCycles')
+    const billingSummaryService =
+        require('../api/billingSummary/billingSummaryService')
+    protectedApi.route('/billingSummary').get(billingSummaryService.getSummary)
 }
