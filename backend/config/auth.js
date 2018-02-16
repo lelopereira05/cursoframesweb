d0975fa711a2c4a634feb09a388348352089b97d
lelopereira05 <lelopereira05@hotmail.com>
2018-02-16 11:06:32 -0200

    Add files via upload

--- backend/config/auth.js
+++ backend/config/auth.js
@@ -8,7 +8,7 @@ module.exports = (req, res, next) =>{
     }else{
         const token = req.body.token || req.query.token || req.headers['authorization']
         if(!token){
-            return res.status(403).send({errors:['No toekn provided.']})
+            return res.status(403).send({errors:['No token provided.']})
         }
         jwt.verify(token, env.authSecret, function(err, decoded){
             if(err){
